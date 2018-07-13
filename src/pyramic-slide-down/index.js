29c87a5de685c3a31bdbfc2d17fba27957d3ee0f
Pham Trong Phat <phat.phamtrong@lazada.com>
2018-07-13 16:43:35 +0800

    feat: update code

--- src/pyramic-slide-down/index.js
+++ src/pyramic-slide-down/index.js
@@ -6,24 +6,7 @@ const input = [
 ];
 
 const pyramicSlideDown = pyramid => {
-  const length = pyramid.length;
-  const root = pyramid[0][0];
-  const loop = arr => {
-    const idx = Math.floor(Math.random() * 2);
-    return {
-      value:  arr[idx],
-      idx,
-    };
-  }
-
-  let result = root;
-  let lastIdx;
-  for (var i = 1; i < pyramid.length; i++) {
-    const floor = loop(pyramid[i]);
-    result += floor.value;
-    lastIdx = idx;
-  }
-  console.log(result);
+  const checkedList = [];
 }
 
 pyramicSlideDown(input);
